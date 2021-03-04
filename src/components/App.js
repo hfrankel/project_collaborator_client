59dfb8b2955ba53f2f3e7157c5e95282444490de
Harry Frankel <hfranks92@gmail.com>
2021-03-04 20:13:50 +1100

    Add dotenv with creds. Add auth0 to client & test component

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,21 @@
 import React from 'react';
+import LoginButton from './LoginButton';
+import LogoutButton from './LogoutButton';
+import Profile from './Profile';
 
 const App = () => {
-  return <div>Hello World</div>;
+  return (
+    <div>
+      <div>Hello World</div>
+      <LoginButton />
+      <LogoutButton />
+      <br />
+      <br />
+      <br />
+      <br />
+      <Profile />
+    </div>
+  );
 };
 
 export default App;
