5e453e0d9038f20700c6024837c8823004f1cf2d
Harry Frankel <hfranks92@gmail.com>
2021-03-04 22:31:45 +1100

    Move auth logic out of navbar

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,11 +3,13 @@ import LoginButton from '../LoginButton/LoginButton';
 import LogoutButton from '../LogoutButton/LogoutButton';
 import Navbar from '../Navbar/Navbar';
 import Profile from '../Profile/Profile';
+import { useAuth0 } from '@auth0/auth0-react';
 
 const App = () => {
+  const { isAuthenticated } = useAuth0();
   return (
     <div>
-      <Navbar />
+      <Navbar>{isAuthenticated ? <LogoutButton /> : <LoginButton />}</Navbar>
       <br />
       <Profile />
     </div>
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,12 +1,7 @@
 import React from 'react';
-import { useAuth0 } from '@auth0/auth0-react';
-import LoginButton from './../LoginButton/LoginButton';
-import LogoutButton from './../LogoutButton/LogoutButton';
 import './Navbar.css';
 
-const Navbar = () => {
-  const { isAuthenticated } = useAuth0();
-
+const Navbar = ({ children }) => {
   return (
     <nav className="navbar">
       <div className="navbar__div-brand">Home</div>
@@ -14,7 +9,7 @@ const Navbar = () => {
         <li className="navbar__li">Projects</li>
         <li className="navbar__li">About</li>
         <li className="navbar__li">Contact</li>
-        {isAuthenticated ? <LogoutButton /> : <LoginButton />}
+        {children}
       </ul>
     </nav>
   );
